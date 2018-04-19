44e5d1652d940b77a37908689f9af4057abb12be
pandaman64 <kointosudesuyo@infoseek.jp>
2018-04-19 16:53:16 +0900

    implement backward ad

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 extern crate typed_arena;
 
+use std::cell::Cell;
+use std::cell::RefCell;
 use std::collections::HashMap;
 
 #[derive(Debug)]
@@ -18,12 +20,126 @@ pub enum NodeType<'a> {
 
 #[derive(Debug)]
 pub struct NodeData<'a> {
-    type_: NodeType<'a>
+    type_: NodeType<'a>,
+    value: Cell<f32>,
+    grads: RefCell<HashMap<String, f32>>,
 }
 
 impl<'a> std::convert::From<NodeType<'a>> for NodeData<'a> {
     fn from(type_: NodeType<'a>) -> Self {
-        NodeData { type_ }
+        NodeData {
+            type_,
+            value: Cell::new(0f32), // bad idea
+            grads: RefCell::new(HashMap::new()),
+        }
+    }
+}
+
+impl<'a> NodeData<'a> {
+    pub fn reset_grads(&self) {
+        use NodeType::*;
+
+        self.grads.borrow_mut().clear();
+        match self.type_ {
+            Const(_) | Var(_) => {},
+            Neg(value) | Pow(value, _) | Sin(value) | Cos(value) => value.reset_grads(),
+            Add(lhs, rhs) | Sub(lhs, rhs) | Mul(lhs, rhs) | Div(lhs, rhs) => {
+                lhs.reset_grads();
+                rhs.reset_grads();
+            },
+        }
+    }
+
+    pub fn backward_ad(&self, variables: &[&str]) {
+        use NodeType::*;
+
+        self.reset_grads();
+
+        // we can detect cyclic dependencies if this borrow_mut fails
+        let mut grads = self.grads.borrow_mut();
+
+        // we already set gradients
+        if grads.len() != 0 {
+            return;
+        }
+
+        // we may be able to denote grad of x is 0 by just leaving grads[x] empty
+        match self.type_ {
+            Const(_) => {
+                for v in variables {
+                    grads.insert(v.to_string(), 0f32);
+                }
+            },
+            Var(ref this) => {
+                for v in variables {
+                    if this == v {
+                        grads.insert(v.to_string(), 1f32);
+                    } else {
+                        grads.insert(v.to_string(), 0f32);
+                    }
+                }
+            },
+            Neg(value) => {
+                value.backward_ad(variables);
+
+                for v in variables {
+                    grads.insert(v.to_string(), -value.grads.borrow()[*v]);
+                }
+            },
+            Add(lhs, rhs) => {
+                lhs.backward_ad(variables);
+                rhs.backward_ad(variables);
+
+                for v in variables {
+                    grads.insert(v.to_string(), lhs.grads.borrow()[*v] + rhs.grads.borrow()[*v]);
+                }
+            },
+            Sub(lhs, rhs) => {
+                lhs.backward_ad(variables);
+                rhs.backward_ad(variables);
+
+                for v in variables {
+                    grads.insert(v.to_string(), lhs.grads.borrow()[*v] - rhs.grads.borrow()[*v]);
+                }
+            },
+            Mul(lhs, rhs) => {
+                lhs.backward_ad(variables);
+                rhs.backward_ad(variables);
+
+                for v in variables {
+                    grads.insert(v.to_string(), lhs.grads.borrow()[*v] * rhs.value.get() + lhs.value.get() * rhs.grads.borrow()[*v]);
+                }
+            },
+            Div(lhs, rhs) => {
+                lhs.backward_ad(variables);
+                rhs.backward_ad(variables);
+
+                for v in variables {
+                    grads.insert(v.to_string(), (lhs.grads.borrow()[*v] * rhs.value.get() - lhs.value.get() * rhs.grads.borrow()[*v]) / (rhs.value.get().powf(2f32)));
+                }
+            },
+            Pow(lhs, rhs) => {
+                lhs.backward_ad(variables);
+
+                for v in variables {
+                    grads.insert(v.to_string(), rhs * self.value.get().powf(rhs - 1f32) * lhs.grads.borrow()[*v]);
+                }
+            },
+            Sin(value) => {
+                value.backward_ad(variables);
+
+                for v in variables {
+                    grads.insert(v.to_string(), self.value.get().cos() * value.grads.borrow()[*v]);
+                }
+            },
+            Cos(value) => {
+                value.backward_ad(variables);
+
+                for v in variables {
+                    grads.insert(v.to_string(), -self.value.get().sin() * value.grads.borrow()[*v]);
+                }
+            },
+        }
     }
 }
 
@@ -88,8 +204,8 @@ fn basic_forward() {
     let arena = Arena::new();
     let arena = &arena;
 
-    let x = var(arena, "x".into());
-    let y = var(arena, "y".into());
+    let x = var(arena, "x".to_string());
+    let y = var(arena, "y".to_string());
 
     let mul = mul(arena, x, y);
     let div = div(arena, x, y);
@@ -98,8 +214,8 @@ fn basic_forward() {
 
     let assignment = {
         let mut assignment = HashMap::new();
-        assignment.insert("x".into(), 8f32);
-        assignment.insert("y".into(), 4f32);
+        assignment.insert("x".to_string(), 8f32);
+        assignment.insert("y".to_string(), 4f32);
         assignment
     };
 
